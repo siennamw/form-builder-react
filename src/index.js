c0b5efe27b3c0175fba5634af02625dd62b08cf6
Sienna <sienna.m.wood@gmail.com>
2018-05-01 09:31:17 -0600

    fix css import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from './redux/store';
-import './index.scss';
+import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
