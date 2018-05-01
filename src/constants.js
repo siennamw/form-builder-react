b353536a82e25786204ea7823e976ee73ea26ad1
Sienna <sienna.m.wood@gmail.com>
2018-05-01 13:32:35 -0600

    fix typo in constants

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const TEXT_FIELD = 'TEXT_FIELD';
 export const RADIO_LIST = 'RADIO_LIST';
-export const RADIO_SUB_ITEM = 'DROPDOWN_SUB_ITEM';
+export const RADIO_SUB_ITEM = 'RADIO_SUB_ITEM';
 export const DROPDOWN_LIST = 'DROPDOWN_LIST';
 export const DROPDOWN_SUB_ITEM = 'DROPDOWN_SUB_ITEM';
 export const SUBMIT_BUTTON = 'SUBMIT_BUTTON';
