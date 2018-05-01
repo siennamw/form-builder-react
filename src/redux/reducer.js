f7468106194e98f87270838a1fb9ab9b0f93cf1d
Sienna <sienna.m.wood@gmail.com>
2018-05-01 09:19:21 -0600

    refactor reducer with formItemOrder

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,38 +2,43 @@ import uuid from 'uuid/v1';
 import * as actionTypes from './actionTypes';
 
 // uuid: {
-//   position: 1,
+//   order: 1,
 //   kind: 'RADIO_LIST',
 //   label: 'Color',
 //   items: {},
 // }
 
 const defaultState = {
-  nextPosition: 0,
   formItems: {},
+  formItemOrder: [],
 };
 
 export default function reducer(state = defaultState, action) {
   switch (action.type) {
     case actionTypes.ADD_FORM_ITEM: {
       const newState = { ...state };
-      newState.formItems = { ...newState.formItems };
+      const id = uuid();
 
-      newState.formItems[uuid()] = {
-        position: newState.nextPosition,
+      newState.formItemOrder = [...newState.formItemOrder, id];
+
+      newState.formItems = { ...newState.formItems };
+      newState.formItems[id] = {
         kind: action.kind,
         label: action.label || '',
         items: action.items || {},
       };
 
-      newState.nextPosition += 1;
       return newState;
     }
     case actionTypes.REMOVE_FORM_ITEM: {
       const newState = { ...state };
-      newState.formItems = { ...newState.formItems };
 
+      newState.formItems = { ...newState.formItems };
       delete newState.formItems[action.id];
+
+      newState.formItemOrder = newState.formItemOrder.filter(id => (
+        id !== action.id
+      ));
 
       return newState;
     }
