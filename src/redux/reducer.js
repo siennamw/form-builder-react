ff3b45be5747640a02eae9ec86162dec85740a3e
Sienna <sienna.m.wood@gmail.com>
2018-05-01 13:26:07 -0600

    reducer for update and subitem actions

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,10 +2,11 @@ import uuid from 'uuid/v1';
 import * as actionTypes from './actionTypes';
 
 // uuid: {
-//   order: 1,
 //   kind: 'RADIO_LIST',
 //   label: 'Color',
-//   items: {},
+//   subItems: [
+//      uuids
+//   ],
 // }
 
 const defaultState = {
@@ -17,7 +18,7 @@ export default function reducer(state = defaultState, action) {
   switch (action.type) {
     case actionTypes.ADD_FORM_ITEM: {
       const newState = { ...state };
-      const id = uuid();
+      const id = action.id || uuid();
 
       newState.formItemOrder = [...newState.formItemOrder, id];
 
@@ -25,8 +26,22 @@ export default function reducer(state = defaultState, action) {
       newState.formItems[id] = {
         kind: action.kind,
         label: action.label || '',
-        items: action.items || {},
+        subItems: action.subItems || [],
       };
+
+      return newState;
+    }
+    case actionTypes.UPDATE_FORM_ITEM: {
+      const newState = { ...state };
+      newState.formItems[action.id] = { ...newState.formItems[action.id] };
+
+      if (action.label !== undefined) {
+        newState.formItems[action.id].label = action.label;
+      }
+
+      if (action.subItems !== undefined) {
+        newState.formItems[action.id].subItems = action.subItems;
+      }
 
       return newState;
     }
@@ -42,6 +57,56 @@ export default function reducer(state = defaultState, action) {
 
       return newState;
     }
+    case actionTypes.ADD_SUB_ITEM: {
+      const newState = { ...state };
+      const id = action.id || uuid();
+
+      newState.formItems = { ...newState.formItems };
+      newState.formItems[id] = {
+        kind: action.kind,
+        label: action.label || '',
+        value: action.value || '',
+        parentId: action.parentId,
+      };
+
+      newState.formItems.subItems.push(id);
+
+      return newState;
+    }
+    case actionTypes.UPDATE_SUB_ITEM: {
+      const newState = { ...state };
+
+      newState.formItems = { ...newState.formItems };
+
+      if (action.label !== undefined) {
+        newState.formItems[action.id].label = action.label;
+      }
+
+      if (action.value !== undefined) {
+        newState.formItems[action.id].value = action.value;
+      }
+
+      return newState;
+    }
+    case actionTypes.REMOVE_SUB_ITEM: {
+      const newState = { ...state };
+
+      newState.formItems = { ...newState.formItems };
+
+      const parent = newState.formItems[action.id].parentId;
+
+      delete newState.formItems[action.id];
+
+      // remove subItem from parent list
+      newState.formItems[parent] = { ...newState.formItems[parent] };
+      newState.formItems[parent].subItems = newState.formItems[parent].subItems.filter(id => (
+        id !== action.id
+      ));
+
+      return newState;
+    }
+
+
     default: {
       return state;
     }
